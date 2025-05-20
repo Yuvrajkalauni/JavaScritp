29147f73007619f0d41f817d7c0676b7141878bb
Yuvrajkalauni <yuvrajkalauni8@gmail.com>
2025-05-20 23:03:55 +0545

    Variables

---
+++ Day-1.js
@@ -0,0 +1,8 @@
+// types of variables in javascritp
+var name = "Yuvraj";
+// another way to add declear variable
+let ourname = "t4" ; // this variable is used to define variable for specific function like local variable
+// another is
+const age = 24 ;  // variable will never change ones fixed
+   console.log(age)
+  
