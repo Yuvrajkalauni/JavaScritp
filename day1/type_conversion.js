b6ad297fdff8889441106d25172356e49356a75a
Yuvrajkalauni <yuvrajkalauni8@gmail.com>
2025-09-08 16:22:43 +0545

    update

--- day1/type_conversion.js
+++ day1/type_conversion.js
@@ -1,4 +1,15 @@
-// Changing data of one type into another type
+// Changing data of one type into another type, it is called type_conversion or type_casting
 let age = prompt("Enter your age:")
+console.log(age)
+console.log(typeof(age))
 age = Number(age)
 console.log(age)
+console.log(typeof(age))
+
+console.log("||||||||||||||||||||||||||||||||||||");
+
+
+let hight = "5.8"
+console.log(typeof(hight))
+hight = Number(hight)
+console.log(typeof(hight))
