2eea3270f05b71c2aabd0c9160bf36f79b5e7afb
Yuvrajkalauni <yuvrajkalauni8@gmail.com>
2025-09-08 14:50:34 +0545

    with html file to run on browser

---
+++ day1/type_coercion.js
@@ -0,0 +1,10 @@
+console.log("1"-2)
+console.log("4"*2)
+console.log("9"/3)
+// -,*./, have only one work,so JS engine change string into integer
+console.log("********************88")
+
+console.log("11"+1)
+console.log(11+1)
+
+//here + have 2 work so one is to concadination and another is addition
---
+++ day1/type_conversion.js
@@ -0,0 +1,4 @@
+// Changing data of one type into another type
+let age = prompt("Enter your age:")
+age = Number(age)
+console.log(age)
