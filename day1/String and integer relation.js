292820fcb64d3a94b6f88571ae5baeee4b014183
Yuvrajkalauni <yuvrajkalauni8@gmail.com>
2025-09-08 13:08:42 +0545

    day1

---
+++ day1/String and integer relation.js
@@ -0,0 +1,25 @@
+// types of variables in javascritp
+var name = "Yuvraj";
+// another way to add declear variable
+let ourname = "t4" ; // this variable is used to define variable for specific function like local variable
+// another is
+const age = 24 ;  // variable will never change ones fixed
+   console.log(age)
+  
+
+
+console.log("****************")
+
+
+   //sum of 2 integer 
+   let a=10 // in JS we can code without using semicolon
+   let b=20
+   let c="30"
+   console.log (a+b)
+   console.log(typeof(a+b))
+   console.log (a+b+c)
+   console.log(typeof(a+b+c))
+   console.log (c+a+b)
+   console.log(typeof(c+a+b))
+
+// example explain the ralation between string and integer
