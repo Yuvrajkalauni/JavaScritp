98e275a92d011603d3d2be6f3042c6210b7135b0
Yuvrajkalauni <yuvrajkalauni8@gmail.com>
2025-09-09 13:55:33 +0545

    afternoon_commit

--- day2/maths_function.js
+++ day2/maths_function.js
@@ -1,5 +1,35 @@
 //Round funtion
 console.log(Math.round(3.14159))
+
 console.log(Math.ceil(3.14159))
 
-console.log(Math.cbrt(3.14159))
+console.log(Math.trunc(3.14159))
+
+console.log(Math.pow(2,3))
+
+console.log(Math.sqrt(4))
+
+console.log(Math.cbrt(64))
+
+console.log(Math.abs(-69))
+
+console.log(Math.max(1,2,3,4,5));
+
+console.log(Math.min(1,2,3,4,5));
+
+console.log(Math.random())
+
+//4_digit otp example
+console.log(Math.trunc(Math.random()*9000+1000))
+
+let a = 3.14159
+console.log(a.toFixed(3));
+console.log(a.toFixed(2));
+console.log(a.toFixed(1));
+console.log(a.toFixed(0));
+
+console.log("||||||||||||||||||||||||||");
+
+let d=10,b=15,c=20 //side's of triangel
+let s=(b+c+d)/2
+console.log(Math.sqrt(s*(s-a)*(s-b)*(s-c)).toFixed(2));
