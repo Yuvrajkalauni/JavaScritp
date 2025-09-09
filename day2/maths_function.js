ec3ce1d505f03730fb635728466b1e68c13c6d74
Yuvrajkalauni <yuvrajkalauni8@gmail.com>
2025-09-09 10:17:15 +0545

    morning_learning

---
+++ day2/maths_function.js
@@ -0,0 +1,5 @@
+//Round funtion
+console.log(Math.round(3.14159))
+console.log(Math.ceil(3.14159))
+
+console.log(Math.cbrt(3.14159))
---
+++ day2/operators.js
@@ -0,0 +1,47 @@
+// We have alreay seen -,+,/,* in other content here let us see %(mode)
+// In division
+console.log(69/4)
+console.log(Math.floor(69/4))
+//In mode 
+console.log(69%4)
+// we get remender 
+console.log(69%10)
+//Here we get last value
+
+
+
+console.log("*******************************")
+
+let a = 30
+b=a //assigment operator
+console.log(a,b)
+console.log(a==b)     //it check left and right value are same or not
+console.log(a=="30")  //fack equla too
+console.log(a==="30") //real equal too
+
+
+console.log("*******************************")
+
+// Unary operator
+//this is example of post operation
+let aa=true
+console.log(aa)
+aa=Number(aa)
+console.log(aa)
+aa++
+console.log(aa)
+console.log("\n")
+//this is example of pre operation
+let bb=false
+console.log(bb)
+bb=Number(bb)
+console.log(bb)
+++bb
+console.log(bb)
+
+
+console.log("*******************************")
+
+// unary operator is not use in constant 
+
+//console.log(++10)
