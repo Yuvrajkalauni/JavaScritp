b897074fd5faf759d34aa5b669d6db406d41d752
Yuvrajkalauni <yuvrajkalauni8@gmail.com>
2025-09-11 00:17:50 +0545

    GEC

---
+++ day3/codeexecution.js
@@ -0,0 +1,14 @@
+
+function square (num){
+    var ans = num * num
+    console.log(ans);
+    return ans
+}
+var square1 = square(2)
+var square = square(4)
+
+/* 
+from this example we can see how javaScript code is executed .
+Java execution contex is created first (memory allocation stage)
+execution context is created in 2 phase
+*/
